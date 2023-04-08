da4286254acbb6bf4001e7b8e3ab21bb230b8947
highpon <s.shiraki.business@gmail.com>
2023-04-08 21:49:16 +0900

    add header and content template

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,4 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
+/* eslint-disable import/no-default-export */
+import { TopPage } from '@/components/page/Top'
 
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Tabs>
-        <TabList>
-          <Tab>One</Tab>
-          <Tab>Two</Tab>
-          <Tab>Three</Tab>
-        </TabList>
-
-        <TabPanels>
-          <TabPanel>
-            <p>one!</p>
-          </TabPanel>
-          <TabPanel>
-            <p>two!</p>
-          </TabPanel>
-          <TabPanel>
-            <p>three!</p>
-          </TabPanel>
-        </TabPanels>
-      </Tabs>
-    </main>
-  )
-}
+export default TopPage
